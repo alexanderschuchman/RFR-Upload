b572b710bc178251235875c7f616de0e9fc29030
BP_Bhagyashree Phadnis <bp_bhagyashree_phadnis@colpal.com>
2021-04-23 12:28:43 +0530

    sorting file based on reason

--- segmentation.py
+++ segmentation.py
@@ -1,8 +1,24 @@
 import openpyxl
 from openpyxl import Workbook
 from openpyxl import load_workbook
+import win32com.client as win32
 
-wb1 = load_workbook("samples/RFR Sample.xlsx")
-ws1 = wb1.worksheets[0]
-
-print(ws1.cell(row = 1, column = 1).value)
+excel = win32.gencache.EnsureDispatch('Excel.Application')
+try:
+    wb = excel.Workbooks("C:/Users/bp_bhagyashree phadn/Downloads/RFR-Upload/samples/RFRSample.xlsx")
+except Exception as e:
+    try:
+        wb = excel.Workbooks.Open("C:/Users/bp_bhagyashree phadn/Downloads/RFR-Upload/samples/RFRSample.xlsx")
+    except Exception as e:
+        print(e)
+try:
+    ws = wb.Worksheets('Sheet1')
+    
+    LastRow = ws.UsedRange.Rows.Count
+    
+    ws.Range('R2:R'+str(LastRow)).Sort(Key1=ws.Range('R1'), Order1=1, Orientation=1)
+    
+    wb.SaveAs(Filename="C:\\Users\\bp_bhagyashree phadn\\Downloads\\RFR-Upload\\samples\\sorted.xlsx")
+    excel.Application.Quit()
+except Exception as e:
+    print(e)
